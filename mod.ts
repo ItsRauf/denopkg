c1e5947b282cd4e8522082ffe31959e69452d1ad
ItsRauf <raufislam2003@gmail.com>
2020-08-04 10:33:43 -0400

    fixes

--- mod.ts
+++ mod.ts
@@ -1,15 +1,12 @@
+import * as commands from "./src/commands/commands.ts";
+
 import { Command } from "https://deno.land/x/cmd/mod.ts";
+
 const cli = new Command("denopkg");
 
 if (import.meta.main) {
-  const denopkgDir = import.meta.url.slice(8, -7);
-  for await (const dirEntry of Deno.readDir(`./src/commands`)) {
-    if (dirEntry.isFile) {
-      const { default: Command } = await import(
-        `./src/commands/${dirEntry.name}`
-      );
-      cli.addCommand(Command);
-    }
-  }
+  Object.values(commands).forEach((cmd) => {
+    cli.addCommand(cmd.default);
+  });
   cli.parse(Deno.args);
 }
---
+++ src/commands/commands.ts
@@ -0,0 +1,4 @@
+export * as addDep from "./add-dep.ts";
+export * as init from "./init.ts";
+export * as run from "./run.ts";
+export * as start from "./start.ts";
